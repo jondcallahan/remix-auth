526addca0fcd45138a4bd7bb58a06b6368bf3785
Jon Callahan <j@joncallahan.com>
2021-12-27 18:34:49 -0600

    chore: prefer cloudflare connecting ip header for determining ip

--- app/utils/net.ts
+++ app/utils/net.ts
@@ -4,15 +4,15 @@ export const badRequest = (data: any, headers?: Headers) =>
   json(data, { status: 400, headers });
 
 const IP_HEADERS = [
+  "CF-Connecting-IP", // Cloudflare
   "Forwarded",
   "Forwarded-For",
-  "X-Forwarded",
+  "X-Forwarded", // May contain an array of IP addresses of proxies, not seeing the real client IP listed
   "X-Forwarded-For", // may contain multiple IP addresses in the format: 'client IP, proxy 1 IP, proxy 2 IP' - we use first one
   "X-Client-IP",
   "X-Real-IP", // Nginx proxy, FastCGI
   "X-Cluster-Client-IP", // Rackspace LB, Riverbed Stingray
   "Proxy-Client-IP",
-  "CF-Connecting-IP", // Cloudflare
   "Fastly-Client-Ip", // Fastly CDN and Firebase hosting header when forwared to a cloud function
   "True-Client-Ip", // Akamai and Cloudflare
   "WL-Proxy-Client-IP",
@@ -30,13 +30,10 @@ const IP_HEADERS = [
 
 export const getRequestIpAddress = (request: Request) => {
   const headers = request.headers;
-  console.log("getRequestIpAddress", headers);
 
   for (const header of IP_HEADERS) {
     const value = headers.get(header);
     if (value) {
-      console.log("Found value at: ", header);
-
       const parts = value.split(/\s*,\s*/g);
       return parts[0] ?? null;
     }
